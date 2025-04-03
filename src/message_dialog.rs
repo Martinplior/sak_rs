b1f6b8c817577f1372119cde00888d6d2b8752a9
Martinplior <martinpliorl@gmail.com>
2025-04-03 16:12:55 +0800

    set_level message should be impl Into<String> add confirm

--- src/message_dialog.rs
+++ src/message_dialog.rs
@@ -1,23 +1,35 @@
 #[must_use]
-pub fn info(message: &str) -> rfd::MessageDialog {
+pub fn info(message: impl Into<String>) -> rfd::MessageDialog {
     rfd::MessageDialog::new()
+        .set_level(rfd::MessageLevel::Info)
         .set_title("信息")
         .set_description(message)
         .set_buttons(rfd::MessageButtons::Ok)
 }
 
 #[must_use]
-pub fn warning(message: &str) -> rfd::MessageDialog {
+pub fn warning(message: impl Into<String>) -> rfd::MessageDialog {
     rfd::MessageDialog::new()
+        .set_level(rfd::MessageLevel::Warning)
         .set_title("警告")
         .set_description(message)
         .set_buttons(rfd::MessageButtons::Ok)
 }
 
 #[must_use]
-pub fn error(message: &str) -> rfd::MessageDialog {
+pub fn error(message: impl Into<String>) -> rfd::MessageDialog {
     rfd::MessageDialog::new()
+        .set_level(rfd::MessageLevel::Error)
         .set_title("错误")
         .set_description(message)
         .set_buttons(rfd::MessageButtons::Ok)
+}
+
+#[must_use]
+pub fn confirm(message: impl Into<String>) -> rfd::MessageDialog {
+    rfd::MessageDialog::new()
+        .set_level(rfd::MessageLevel::Info)
+        .set_title("确认")
+        .set_description(message)
+        .set_buttons(rfd::MessageButtons::OkCancel)
 }
